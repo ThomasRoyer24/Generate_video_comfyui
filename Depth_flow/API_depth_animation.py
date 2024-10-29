a924ee1d759a5c3b86c47b86ba11e7682ac67a46
Thomas Royer <thomas.royer24@gmail.com>
2024-10-29 23:16:49 +0100

    API user for depth musique

--- Depth_flow/API_depth_animation.py
+++ Depth_flow/API_depth_animation.py
@@ -8,7 +8,7 @@ import urllib.parse
 import os
 import time
 import math
-
+from Comfy_node_data import Comfy_node
 
 server_address = "127.0.0.1:8188"
 client_id = str(uuid.uuid4())
@@ -43,7 +43,7 @@ def get_video(ws, prompt):
     return history["outputs"]["5"]["gifs"][0]["filename"] # print history to find the filename
 
 
-def generate_video(image_link,duration,user_name,timestamp,motion_component=False,motion_component_param=False,motion_preset=False,motion_preset_param=False,effect=False,effect_param=False,model="depth_anything_v2_vits_fp32.safetensors"):
+def generate_video(image_link,duration,user_name,timestamp,musique_param=False,motion_component_param=False,motion_preset_param=False,effect=False,effect_param=False,model="depth_anything_v2_vits_fp32.safetensors"):
 
     #load workflow in text
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -52,16 +52,18 @@ def generate_video(image_link,duration,user_name,timestamp,motion_component=Fals
     with open(workflow_json, "r+", encoding="utf-8") as fichier:
         base_json = fichier.read()
 
-
     # modifier le fichier json pour le workflow    
-    if motion_component:
-        base_json += Motion_component[motion_component]
+    if motion_component_param:
+        base_json += Comfy_node[motion_component_param[0][1]] # Pour acceder a la 2 eme valeur du premier tuple 
 
-    if motion_preset:
-        base_json += Motion_preset[motion_preset]
+    if motion_preset_param:
+        base_json += Comfy_node[motion_preset_param[0][1]] # Pour acceder a la 2 eme valeur du premier tuple 
 
     if effect:
-        base_json += Effect[effect]
+        base_json += Comfy_node[effect]
+
+    if musique_param:
+        base_json += Comfy_node["Musique"]        
 
     base_json += "}" # close 
 
@@ -86,30 +88,35 @@ def generate_video(image_link,duration,user_name,timestamp,motion_component=Fals
     if effect_param:
         for param_name, param_value in effect_param:
             comfy_json["3"]["inputs"][param_name] = param_value
+    
+    if musique_param:
+        comfy_json["204"]["inputs"]["audio"]= musique_param["musique_link"]
+        comfy_json["204"]["inputs"]["start_time"]= musique_param["start_time"]
+        comfy_json["204"]["inputs"]["duration"]= duration
+        comfy_json["207"]["inputs"]["amount"]= duration*30 #30 frames/sec
 
 
     #Set connection nodes
-    if motion_component:
+    if motion_component_param:
         comfy_json["91"]["inputs"]["motion"] = ["2",0]
 
-    if motion_preset:
-        if motion_component:
+    if motion_preset_param:
+        if motion_component_param:
             comfy_json["2"]["inputs"]["motion"] = ["1",0]
         else : 
             comfy_json["91"]["inputs"]["motion"] = ["1",0]
         
     if effect:
         comfy_json["91"]["inputs"]["effects"] = ["3",0]
+    
+    if musique_param:
+        comfy_json["207"]["inputs"]["image"]= ["50",0]
+        comfy_json["2"]["inputs"]["feature"] = ["209",0]
+        comfy_json["5"]["inputs"]["audio"] = ["204",0]
 
 
     #Set output node
     comfy_json["5"]["inputs"]["filename_prefix"] = user_name+"/"+str(timestamp)+"/"+"Video"
-
-
-        # Sauvegarder le JSON modifié
-    output_json_path = os.path.join(script_dir, f'modified_workflow_{timestamp}.json')
-    with open(output_json_path, "w", encoding="utf-8") as fichier:
-        json.dump(base_json, fichier, indent=4)
 
     ws = websocket.WebSocket()
     ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
@@ -118,276 +125,10 @@ def generate_video(image_link,duration,user_name,timestamp,motion_component=Fals
     
     return video_link
 
-Motion_component = {
-    "Sine": ''',"2": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "amplitude",
-            "feature_mode": "relative",
-            "target": "Isometric",
-            "amplitude": 1,
-            "cycles": 1,
-            "phase": 0,
-            "reverse": false,
-            "bias": 0,
-            "cumulative": false
-        },
-        "class_type": "DepthflowMotionSine",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Sine"
-        }
-    }''',
-
-    "Cosine": ''',"2": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "amplitude",
-            "feature_mode": "relative",
-            "target": "Isometric",
-            "amplitude": 1,
-            "cycles": 1,
-            "phase": 0,
-            "reverse": false,
-            "bias": 0,
-            "cumulative": false
-        },
-        "class_type": "DepthflowMotionCosine",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Cosine"
-        }
-    }''',
-
-    "Linear": ''',"2": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "start",
-            "feature_mode": "relative",
-            "target": "Isometric",
-            "start": 0,
-            "end": 1,
-            "low": 0,
-            "high": 1,
-            "exponent": 1,
-            "reverse": false,
-            "cumulative": false
-        },
-        "class_type": "DepthflowMotionLinear",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Linear"
-        }
-    }''',
-
-    "Exponential": ''',"2": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "base",
-            "feature_mode": "relative",
-            "target": "Isometric",
-            "base": 2,
-            "scale": 1,
-            "reverse": false,
-            "cumulative": false
-        },
-        "class_type": "DepthflowMotionExponential",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Exponential"
-        }
-    }''',
-
-    "Arc": ''',"2": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "start",
-            "feature_mode": "relative",
-            "target": "OffsetX",
-            "start": -0.5,
-            "middle": -0.25,
-            "end": 1,
-            "reverse": false,
-            "cumulative": false
-        },
-        "class_type": "DepthflowMotionArc",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Arc"
-        }
-    }''',
-
-    "Target": ''',"2": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "value",
-            "feature_mode": "relative",
-            "target": "Height",
-            "value": 0.2
-        },
-        "class_type": "DepthflowMotionSetTarget",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Set Target"
-        }
-    }'''
-}
-
-# Dictionnaire de données
-Motion_preset = {
-    "Circle": ''',"1": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "intensity",
-            "feature_mode": "relative",
-            "intensity": 0.65,
-            "reverse": false,
-            "smooth": true,
-            "phase_x": 0,
-            "phase_y": 0,
-            "phase_z": 0,
-            "amplitude_x": 1,
-            "amplitude_y": 1,
-            "amplitude_z": 0,
-            "static_value": 0.3
-        },
-        "class_type": "DepthflowMotionPresetCircle",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Preset Circle"
-        }
-    }''',
-
-    "Dolly": ''',"1": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "intensity",
-            "feature_mode": "relative",
-            "intensity": 0.8,
-            "reverse": false,
-            "smooth": true,
-            "loop": true,
-            "depth": 0.5
-        },
-        "class_type": "DepthflowMotionPresetDolly",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Preset Dolly"
-        }
-    }''',
-
-    "Horizontal": ''',"1": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "intensity",
-            "feature_mode": "relative",
-            "intensity": 0.8,
-            "reverse": false,
-            "loop": true,
-            "smooth": true,
-            "phase": 0,
-            "steady_value": 0.3
-        },
-        "class_type": "DepthflowMotionPresetHorizontal",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Preset Horizontal"
-        }
-    }''',
-
-    "Vertical": ''',"1": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "intensity",
-            "feature_mode": "relative",
-            "intensity": 0.8,
-            "reverse": false,
-            "loop": true,
-            "smooth": true,
-            "phase": 0,
-            "steady_value": 0.3
-        },
-        "class_type": "DepthflowMotionPresetVertical",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Preset Vertical"
-        }
-    }''',
-
-    "Zoom": ''',"1": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "intensity",
-            "feature_mode": "relative",
-            "intensity": 0.8,
-            "reverse": false,
-            "smooth": true,
-            "phase": 0,
-            "loop": false
-        },
-        "class_type": "DepthflowMotionPresetZoom",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Preset Zoom"
-        }
-    }''',
-
-    "Orbital": ''',"1": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "intensity",
-            "feature_mode": "relative",
-            "intensity": 0.8,
-            "reverse": false,
-            "depth": 0.5
-        },
-        "class_type": "DepthflowMotionPresetOrbital",
-        "_meta": {
-            "title": "🌊 Depthflow Motion Preset Orbital"
-        }
-    }'''
-}
-
-Effect = {
-    "DOF": ''',"3": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "dof_intensity",
-            "feature_mode": "absolute",
-            "dof_enable": true,
-            "dof_start": 0.3,
-            "dof_end": 1,
-            "dof_exponent": 2,
-            "dof_intensity": 1,
-            "dof_quality": 16,
-            "dof_directions": 16
-        },
-        "class_type": "DepthflowEffectDOF",
-        "_meta": {
-            "title": "🌊 Depthflow Effect DOF"
-        }
-    }''',
-
-    "Vignette": ''',"3": {
-        "inputs": {
-            "strength": 1,
-            "feature_threshold": 0,
-            "feature_param": "vignette_intensity",
-            "feature_mode": "relative",
-            "vignette_enable": true,
-            "vignette_intensity": 30,
-            "vignette_decay": 0.8
-        },
-        "class_type": "DepthflowEffectVignette",
-        "_meta": {
-            "title": "🌊 Depthflow Effect Vignette"
-        }
-    }'''
-}
 
 
 if __name__ == "__main__":
 
-    print(generate_video("ComfyUI_00046_.png",8,"test","17181381",motion_component="Sine",motion_component_param=[("target","Zoom"),("bias","1"),("amplitude","0.3")],motion_preset = "Orbital"))
+    musique ={"musique_link":"audio_cut.MP3","start_time":"10"}
+
+    print(generate_video("35393166.png",7,"test","17181381",musique_param=musique,motion_component_param=[("type","Sine"),("target","Zoom"),("bias","1"),("amplitude","0.3")],motion_preset_param=[("type","Orbital")]))
---
+++ Depth_flow/Comfy_node_data.py
@@ -0,0 +1,370 @@
+Comfy_node = {
+
+#Motion component
+
+    "Sine": ''',"2": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "amplitude",
+            "feature_mode": "relative",
+            "target": "Zoom",
+            "amplitude": 1,
+            "cycles": 1,
+            "phase": 0,
+            "reverse": false,
+            "bias": 0,
+            "cumulative": false
+        },
+        "class_type": "DepthflowMotionSine",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Sine"
+        }
+    }''',
+
+    "Cosine": ''',"2": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "amplitude",
+            "feature_mode": "relative",
+            "target": "Isometric",
+            "amplitude": 1,
+            "cycles": 1,
+            "phase": 0,
+            "reverse": false,
+            "bias": 0,
+            "cumulative": false
+        },
+        "class_type": "DepthflowMotionCosine",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Cosine"
+        }
+    }''',
+
+    "Linear": ''',"2": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "start",
+            "feature_mode": "relative",
+            "target": "Isometric",
+            "start": 0,
+            "end": 1,
+            "low": 0,
+            "high": 1,
+            "exponent": 1,
+            "reverse": false,
+            "cumulative": false
+        },
+        "class_type": "DepthflowMotionLinear",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Linear"
+        }
+    }''',
+
+    "Exponential": ''',"2": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "base",
+            "feature_mode": "relative",
+            "target": "Isometric",
+            "base": 2,
+            "scale": 1,
+            "reverse": false,
+            "cumulative": false
+        },
+        "class_type": "DepthflowMotionExponential",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Exponential"
+        }
+    }''',
+
+    "Arc": ''',"2": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "start",
+            "feature_mode": "relative",
+            "target": "OffsetX",
+            "start": -0.5,
+            "middle": -0.25,
+            "end": 1,
+            "reverse": false,
+            "cumulative": false
+        },
+        "class_type": "DepthflowMotionArc",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Arc"
+        }
+    }''',
+
+    "Target": ''',"2": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "value",
+            "feature_mode": "relative",
+            "target": "Height",
+            "value": 0.2
+        },
+        "class_type": "DepthflowMotionSetTarget",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Set Target"
+        }
+    }''',
+
+# Motion preset
+
+    "Circle": ''',"1": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "intensity",
+            "feature_mode": "relative",
+            "intensity": 0.65,
+            "reverse": false,
+            "smooth": true,
+            "phase_x": 0,
+            "phase_y": 0,
+            "phase_z": 0,
+            "amplitude_x": 1,
+            "amplitude_y": 1,
+            "amplitude_z": 0,
+            "static_value": 0.3
+        },
+        "class_type": "DepthflowMotionPresetCircle",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Preset Circle"
+        }
+    }''',
+
+    "Dolly": ''',"1": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "intensity",
+            "feature_mode": "relative",
+            "intensity": 0.8,
+            "reverse": false,
+            "smooth": true,
+            "loop": true,
+            "depth": 0.5
+        },
+        "class_type": "DepthflowMotionPresetDolly",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Preset Dolly"
+        }
+    }''',
+
+    "Horizontal": ''',"1": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "intensity",
+            "feature_mode": "relative",
+            "intensity": 0.8,
+            "reverse": false,
+            "loop": true,
+            "smooth": true,
+            "phase": 0,
+            "steady_value": 0.3
+        },
+        "class_type": "DepthflowMotionPresetHorizontal",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Preset Horizontal"
+        }
+    }''',
+
+    "Vertical": ''',"1": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "intensity",
+            "feature_mode": "relative",
+            "intensity": 0.8,
+            "reverse": false,
+            "loop": true,
+            "smooth": true,
+            "phase": 0,
+            "steady_value": 0.3
+        },
+        "class_type": "DepthflowMotionPresetVertical",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Preset Vertical"
+        }
+    }''',
+
+    "Zoom": ''',"1": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "intensity",
+            "feature_mode": "relative",
+            "intensity": 0.8,
+            "reverse": false,
+            "smooth": true,
+            "phase": 0,
+            "loop": false
+        },
+        "class_type": "DepthflowMotionPresetZoom",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Preset Zoom"
+        }
+    }''',
+
+    "Orbital": ''',"1": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "intensity",
+            "feature_mode": "relative",
+            "intensity": 0.8,
+            "reverse": false,
+            "depth": 0.5
+        },
+        "class_type": "DepthflowMotionPresetOrbital",
+        "_meta": {
+            "title": "🌊 Depthflow Motion Preset Orbital"
+        }
+    }''',
+
+    "DOF": ''',"3": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "dof_intensity",
+            "feature_mode": "absolute",
+            "dof_enable": true,
+            "dof_start": 0.3,
+            "dof_end": 1,
+            "dof_exponent": 2,
+            "dof_intensity": 1,
+            "dof_quality": 16,
+            "dof_directions": 16
+        },
+        "class_type": "DepthflowEffectDOF",
+        "_meta": {
+            "title": "🌊 Depthflow Effect DOF"
+        }
+    }''',
+
+    "Vignette": ''',"3": {
+        "inputs": {
+            "strength": 1,
+            "feature_threshold": 0,
+            "feature_param": "vignette_intensity",
+            "feature_mode": "relative",
+            "vignette_enable": true,
+            "vignette_intensity": 30,
+            "vignette_decay": 0.8
+        },
+        "class_type": "DepthflowEffectVignette",
+        "_meta": {
+            "title": "🌊 Depthflow Effect Vignette"
+        }
+    }''',
+
+
+# Musique workflow
+
+    "Musique":''',"204": {
+    "inputs": {
+      "audio": "",
+      "start_time": 0,
+      "duration": 0
+    },
+    "class_type": "VHS_LoadAudioUpload",
+    "_meta": {
+      "title": "Load Audio (Upload)🎥🅥🅗🅢"
+    }
+  },
+  "205": {
+    "inputs": {
+      "frame_rate": 30,
+      "model": [
+        "206",
+        0
+      ],
+      "audio": [
+        "204",
+        0
+      ],
+      "video_frames": [
+        "207",
+        0
+      ]
+    },
+    "class_type": "AudioSeparator",
+    "_meta": {
+      "title": "Audio Separator | RyanOnTheInside"
+    }
+  },
+  "206": {
+    "inputs": {
+      "model_name": "umxl"
+    },
+    "class_type": "DownloadOpenUnmixModel",
+    "_meta": {
+      "title": "Download Open Unmix Model | RyanOnTheInside"
+    }
+  },
+  "207": {
+    "inputs": {
+      "amount": 0,
+      "image": [
+        "",
+        0
+      ]
+    },
+    "class_type": "RepeatImageBatch",
+    "_meta": {
+      "title": "RepeatImageBatch"
+    }
+  },
+  "208": {
+    "inputs": {
+      "extraction_method": "amplitude_envelope",
+      "audio": [
+        "205",
+        1
+      ],
+      "feature_pipe": [
+        "205",
+        5
+      ]
+    },
+    "class_type": "AudioFeatureExtractor",
+    "_meta": {
+      "title": "Audio Feature & Extractor | RyanOnTheInside"
+    }
+  },
+  "209": {
+    "inputs": {
+      "smoothing_type": "moving_average",
+      "window_size": 5,
+      "alpha": 0.3,
+      "sigma": 1,
+      "invert_output": false,
+      "feature": [
+        "208",
+        0
+      ]
+    },
+    "class_type": "FeatureSmoothing",
+    "_meta": {
+      "title": "FeatureMod Smoothing | RyanOnTheInside"
+    }
+  },
+  "210": {
+    "inputs": {
+      "image": "35393166.png",
+      "upload": "image"
+    },
+    "class_type": "LoadImage",
+    "_meta": {
+      "title": "Load Image"
+    }
+  }'''
+}
